f6fd4b6ecf4ec088e72bb6f25749ebbb0fbeec19
kc2gug <kc2gug@users.noreply.github.com>
2020-04-30 22:57:25 -0400

    Fixed the reverse mapping

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@
 PORT=3030
 SIMPORT=8000
 GPIO_DRIVE_PINS=(33,11,13,15)
-DRIVE_DIR="FORWARD"
+DRIVE_DIR="REVERSE"
 DRIVE_DEFS={
     "FORWARD": [{
         "FWD":[{"PIN1": True, "PIN2": False,"PIN3": True, "PIN4": False}],
@@ -17,14 +17,14 @@ DRIVE_DEFS={
         "STP":[{"PIN1": False,"PIN2": False,"PIN3": False,"PIN4": False}]}]
     ,
     "REVERSE": [{
-        "FWD":[{"PIN1": True, "PIN2": False,"PIN3": True, "PIN4": False}],
-        "BAK":[{"PIN1": False,"PIN2": True, "PIN3": False,"PIN4": True }],
-        "SRT":[{"PIN1": False,"PIN2": True, "PIN3": True, "PIN4": False}],
-        "SLT":[{"PIN1": True, "PIN2": False,"PIN3": False,"PIN4": True }],
-        "FWL":[{"PIN1": True, "PIN2": False,"PIN3": False,"PIN4": False}],
-        "BWL":[{"PIN1": False,"PIN2": False,"PIN3": False,"PIN4": True }],
-        "BWR":[{"PIN1": False,"PIN2": True, "PIN3": False,"PIN4": False}],
-        "FWR":[{"PIN1": False,"PIN2": False,"PIN3": True, "PIN4": False}],
+        "BAK":[{"PIN1": True, "PIN2": False,"PIN3": True, "PIN4": False}],
+        "FWD":[{"PIN1": False,"PIN2": True, "PIN3": False,"PIN4": True }],
+        "SLT":[{"PIN1": False,"PIN2": True, "PIN3": True, "PIN4": False}],
+        "SRT":[{"PIN1": True, "PIN2": False,"PIN3": False,"PIN4": True }],
+        "BWL":[{"PIN1": True, "PIN2": False,"PIN3": False,"PIN4": False}],
+        "FWL":[{"PIN1": False,"PIN2": False,"PIN3": False,"PIN4": True }],
+        "FWR":[{"PIN1": False,"PIN2": True, "PIN3": False,"PIN4": False}],
+        "BWR":[{"PIN1": False,"PIN2": False,"PIN3": True, "PIN4": False}],
         "STP":[{"PIN1": False,"PIN2": False,"PIN3": False,"PIN4": False}]}]
     }
 
