d84d542cac99f92540a1e075e056fc0466084916
kc2gug <kc2gug@users.noreply.github.com>
2020-04-30 22:53:43 -0400

    Data file needed to be fixed, set instead of list

--- app/testing.py
+++ app/testing.py
@@ -6,7 +6,10 @@ import config as cfg
 
 print (len(cfg.GPIO_DRIVE_PINS))
 print (cfg.GPIO_DRIVE_PINS)
-print (cfg.DRIVE_DEFS['FORWARD'][0]["FWD"])
+print (cfg.DRIVE_DEFS['FORWARD'][1])
+print (cfg.DRIVE_DEFS['FORWARD'][0]["SRT"])
+
+direction="BAK"
 
 for thing in range(0,len(cfg.GPIO_DRIVE_PINS)):
-    print (cfg.GPIO_DRIVE_PINS[thing],cfg.DRIVE_DEFS['FORWARD'][0]["FWD"][0]["PIN"+str(thing+1)])
+    print (cfg.GPIO_DRIVE_PINS[thing],cfg.DRIVE_DEFS['FORWARD'][0][str(direction)][0]["PIN"+str(thing+1)])
